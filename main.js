e49c76375753d851faff007f133098b0ee869cdd
Mac51290 <machmachi.charaf@gmail.com>
2025-04-12 03:53:41 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BiteconHome from "./BiteconHome";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BiteconHome />
+  </React.StrictMode>
+);
