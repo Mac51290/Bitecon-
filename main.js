39e264ca5773bf73d51a0cea47033b0dd3a87974
Mac51290 <machmachi.charaf@gmail.com>
2025-04-12 04:46:27 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BiteconHome from "./BiteconHome.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BiteconHome />
-  </React.StrictMode>
-);
