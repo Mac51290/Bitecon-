63111a94fea888a965ff9a299c904f2061d64728
Mac51290 <machmachi.charaf@gmail.com>
2025-04-12 04:35:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BiteconHome from "./BiteconHome";
+import BiteconHome from "./BiteconHome.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
